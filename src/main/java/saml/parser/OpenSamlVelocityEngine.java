679d207277463d73acbe4ee99932044a1610e81b
Okke Harsta <oharsta@zilverline.com>
2023-11-06 08:45:42 +0100

    Velocity tenplates do not change

--- src/main/java/saml/parser/OpenSamlVelocityEngine.java
+++ src/main/java/saml/parser/OpenSamlVelocityEngine.java
@@ -12,14 +12,21 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class OpenSamlVelocityEngine {
 
+    private final org.apache.velocity.app.VelocityEngine velocityEngine;
+
+    public OpenSamlVelocityEngine() {
+        this.velocityEngine = VelocityEngine.newVelocityEngine();
+        velocityEngine.setProperty("runtime.log.instance", NOPLogger.NOP_LOGGER);
+        velocityEngine.setProperty("velocimacro.library.autoreload", false);
+        velocityEngine.setProperty("resource.loader.file.cache", true);
+        velocityEngine.setProperty("resource.loader.file.modificationCheckInterval", -1);
+        velocityEngine.init();
+    }
+
     public void process(String templateId,
                         Map<String, Object> model,
                         Writer out) {
-        org.apache.velocity.app.VelocityEngine velocityEngine = VelocityEngine.newVelocityEngine();
-        velocityEngine.setProperty("runtime.log.instance", NOPLogger.NOP_LOGGER);
-        velocityEngine.init();
-        VelocityContext context = new VelocityContext();
-        model.forEach(context::put);
+        VelocityContext context = new VelocityContext(model);
         velocityEngine.mergeTemplate(templateId, UTF_8.name(), context, out);
     }
 
